d879384c3d031c6e2a5481cb47e262e0314410d1
lzaql <127473818+lzaql@users.noreply.github.com>
2023-03-24 10:10:15 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def page_index():
+    return "Главная страничка"
+
+@app.route("/feed")
+def page_feed():
+        return "Лента новостей"
+
+@app.route("/messages")
+def page_messages():
+    return "Сообщения пользователя"
+
+@app.route('/users/<uid>')
+def page_profile(uid):
+    return f'<h1>Профиль {uid}</h1>'
+
+@app.route('/catalog/items/<itemid>')
+def profile(itemid):
+    return f'<h1>Страничка товара {itemid}</h1>'
+
+app.run(host='127.0.0.21', port='80', debug=True)
